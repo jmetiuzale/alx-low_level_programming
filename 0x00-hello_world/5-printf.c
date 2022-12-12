88e3e532eb9c388820009767863c8221c1eda10a
jmetiuzale <jmetiuzale360@gmail.com>
2022-12-11 22:05:07 -0800

    alx task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - prints the phrase with the proper grammar,
+ * but the outcome is a piece of art
+ * Return: Always 0 (Success)
+ *
+ * */
+int main(void)
+
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}

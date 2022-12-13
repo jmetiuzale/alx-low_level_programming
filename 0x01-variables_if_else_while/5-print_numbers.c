87589603cc8b0e6a09bb45d5d03e8be9996a9aef
jmetiuzale <jmetiuzale360@gmail.com>
2022-12-12 21:08:38 -0800

    alx task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf"%d", num);
+	putchar('\n');
+	return (0)
+}
